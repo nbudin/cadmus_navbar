266d7feda1972fb6a1bce1fca838e2d11a56aab6
Nat Budin <nbudin@patientslikeme.com>
2018-04-04 08:27:25 -0700

    Do fewer SQL queries when rendering the navigation bar

--- lib/cadmus_navbar/renderer.rb
+++ lib/cadmus_navbar/renderer.rb
@@ -2,21 +2,30 @@ module CadmusNavbar
   class Renderer
     include ActionView::Helpers::OutputSafetyHelper
 
-    attr_accessor :request, :current_uri, :root_items
+    attr_accessor :request, :current_uri, :root_items, :items_by_section_id
 
-    def initialize(request:, url_for_page:, root_items: nil)
+    def initialize(request:, url_for_page:, items_by_section_id: nil, root_items: nil)
       @_url_for_page = url_for_page
       @request = request
       @current_uri = URI.parse(request.url)
-      @root_items = root_items
+      @items_by_section_id = items_by_section_id
+
+      if !items_by_section_id && root_items
+        @items_by_section_id = root_items.flat_map { |item| [item, *item.navigation_links] }
+          .group_by(&:navigation_section_id)
+      end
     end
 
     def self.from_parent(parent:, **args)
       items = CadmusNavbar.navigation_item_model.where(
         parent_id: parent.id,
         parent_type: parent.class.base_class.name
-      ).root.includes(:page, navigation_links: :page)
-      self.new(root_items: items.to_a, **args)
+      ).root.includes(:page).group_by(&:navigation_section_id)
+      self.new(items_by_section_id: items.to_a, **args)
+    end
+
+    def root_items
+      items_by_section_id[nil]
     end
 
     def render_navigation_items
@@ -26,8 +35,10 @@ module CadmusNavbar
 
     def render_navigation_item(item)
       case item.item_type
-      when 'section' then render_navigation_section(item, item.navigation_links.sort_by(&:position))
-      when 'link' then render_navigation_link(item, root: true)
+      when 'section'
+        render_navigation_section(item, items_by_section_id[item.id].sort_by(&:position))
+      when 'link'
+        render_navigation_link(item, root: true)
       end
     end
 
